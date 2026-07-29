cf1633897fa605464a60180182a260404f5cb846
marosmester <mestermaros@gmail.com>
2026-07-29 17:35:59 +0200

    QoL: Add second optional argument to fetch_bag.sh

--- bags/fetch_bag.sh
+++ bags/fetch_bag.sh
@@ -6,9 +6,10 @@ REMOTE_BAGS_DIR="/home/robot/bags"
 LOCAL_BAGS_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 if [[ $# -lt 1 ]]; then
-    echo "Usage: $0 <bag_name>"
+    echo "Usage: $0 <bag_name> [destination_dir]"
     exit 1
 fi
 
 BAG_NAME="$1"
-scp -r "${REMOTE_HOST}:${REMOTE_BAGS_DIR}/${BAG_NAME}" "${LOCAL_BAGS_DIR}/"
+DEST_DIR="${2:-$LOCAL_BAGS_DIR}"
+scp -r "${REMOTE_HOST}:${REMOTE_BAGS_DIR}/${BAG_NAME}" "${DEST_DIR}/"
